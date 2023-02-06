46edbce1b13a2fe046d7a70b016ac9d9e55cb404
Daniel Vogel <Daniel.Vogel@vogelfree.software>
2023-02-06 09:49:53 +0100

    Simple console ui menu

--- src/todo-console/Program.cs
+++ src/todo-console/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿RenderHeader();
+var command = RenderMenu();
+
+void RenderHeader() => 
+    Console.WriteLine(
+        $"|#############################################|{Environment.NewLine}" + 
+        $"|               To-Do App                     |{Environment.NewLine}" +
+        $"|#############################################|");
+
+int RenderMenu()
+{
+    Console.Write(
+        $"Wählen Sie zwischen verschiedenen Aktionen:{Environment.NewLine}" +
+        $"  (1) Eintrag erstellen.{Environment.NewLine}" +
+        $"  (2) Eintrag löschen.{Environment.NewLine}" +
+        $"  (3) Eintrag ändern.{Environment.NewLine}" +
+        $"  (4) Alle Einträge löschen.{Environment.NewLine}" +
+        $"  (5) Sortieren.{Environment.NewLine}" +
+        $"  (6) Alle Einträge anzeigen.{Environment.NewLine}" +
+        $"  (7) Liste speichern.{Environment.NewLine}" +
+        $"  (8) Ende.{Environment.NewLine}" +
+        $"Auswahl:");
+    return int.Parse(Console.ReadLine());
+}
